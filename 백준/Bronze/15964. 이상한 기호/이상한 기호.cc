20a5e8bfd5bbd8293753e3406c8c562d6d67179e
taeyeonlee1 <h2ymb5@gmail.com>
2025-04-30 16:47:22 +0900

    [Bronze V] Title: 이상한 기호, Time: 0 ms, Memory: 1112 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15964. 이상한 기호/이상한 기호.cc
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.cc
@@ -1,20 +1,12 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-int sum;
-
-int ky(int a, int b)
-{
-	sum = (a + b) * (a - b);
-	return sum;
-}
 int main()
 {
-	int n, m;
-	scanf("%d %d", &n, &m);
-
-	ky(n, m);
-	printf("%d", sum);
+	long long int n, m;
+	scanf("%lld %lld", &n, &m);
+	printf("%lld", n*n-m*m);
+	
 
 }
 
