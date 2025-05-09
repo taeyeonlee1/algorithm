ae220fb2e83a6a6870a192961f1a3cd39fa30dff
taeyeonlee1 <h2ymb5@gmail.com>
2025-05-09 14:20:21 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.cc
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+int main()
+{
+	int a;
+
+	scanf("%d", &a);
+
+	for (int i = a; i > 0; i--)
+	{
+		for (int j = i; j > 0; j--)
+		{
+		 printf("*");
+
+		}
+
+		printf("\n");
+	}
+
+	
+	
+}
+
+
