a34afbdc7be998f836a1382c0c5eaf6dac684352
taeyeonlee1 <h2ymb5@gmail.com>
2025-05-11 13:21:46 +0900

    [Bronze V] Title: 피갤컵, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/33612. 피갤컵/피갤컵.cc
@@ -0,0 +1,37 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int n;
+
+	scanf("%d", &n);
+
+	switch (n)
+	{
+		case 1: 
+		 printf("2024 8");
+		 break;
+
+		case 2:
+			printf("2025 3");
+			break;
+
+		case 3:
+			printf("2025 10");
+			break;
+
+		case 4:
+			printf("2026 5");
+			break;
+
+		case 5:
+			printf("2026 12");
+			break;
+
+
+		
+
+	}
+	
+}
